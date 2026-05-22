4c07bb7c43f2951d881dff0432293a0cae673504
WhosArjun <arjun.arunkumar.v@gmail.com>
2026-05-22 18:29:56 -0400

    trapezodal execute stuff

--- src/main/java/frc/robot/commands/Trapezoidal.java
+++ src/main/java/frc/robot/commands/Trapezoidal.java
@@ -1,6 +1,10 @@
 package frc.robot.commands;
 
-public class Trapezoidal {
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Drivetrain;
+
+public class Trapezoidal extends Command{
+    private Drivetrain drivetrain;
     private double MAX_SPEED;
     private double MAX_ACCEL;
     private double distance;
@@ -12,7 +16,7 @@ public class Trapezoidal {
     private double t;
     private double t1;
     
-    public Trapezoidal(double MAX_SPEED, double MAX_ACCEL, double distance, double goalX
+    public Trapezoidal(Drivetrain drivetrain,double MAX_SPEED, double MAX_ACCEL, double distance, double goalX
                     , double Ta, double twoTriangleArea, double Tmax, double areaOfRectangle, double t1, double t){
         this.MAX_SPEED = MAX_SPEED;
         this.MAX_ACCEL = MAX_ACCEL;
@@ -24,9 +28,8 @@ public class Trapezoidal {
         this.areaOfRectangle = Tmax * MAX_SPEED;
         this.t1 = Ta+Tmax;
         this.t = t;
+        this.drivetrain = drivetrain;
     }
-    double trapezoidArea = twoTriangleArea+areaOfRectangle; //Area of the trapezoid 
-
     //MAX_SPEED
     public double getMaxSpeed(){
         return MAX_SPEED;
@@ -51,7 +54,27 @@ public class Trapezoidal {
     public double getTime(){
         return t;
     }
-        
+    double accelerationFunction;
+    double pieceWiseFunctionThree;
+    double decelerationFunction;
+    public double speed(){
+        if(getTime()<getTa()){//time is wrong
+            accelerationFunction = getMaxAccel()*getTime();
+            return accelerationFunction;
+        }
+        else if(getTime()<getTmax()){// also wrong
+            pieceWiseFunctionThree = getMaxSpeed();
+            return pieceWiseFunctionThree;
+        }
+        else if(getTime()>getTa()+getTmax()){//also wrong baka
+            decelerationFunction = -(getMaxAccel())*(getTime()-getT1())+getMaxSpeed();
+            return decelerationFunction;
+        }
+        return 0;
+    }
+    public void execute(){
+        speed();
+    }
         
       
     
