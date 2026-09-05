dd75f45168c8913882b0e449a25dd628e7bf1ffd
WhosArjun <arjun.arunkumar.v@gmail.com>
2026-09-05 19:05:54 -0400

    shooter class

---
+++ src/main/java/frc/robot/subsystems/Shooter/Shooter.java
@@ -0,0 +1,8 @@
+package frc.robot.subsystems.Shooter;
+import com.ctre.phoenix6.hardware.TalonFX;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Shooter extends SubsystemBase{
+    
+}
